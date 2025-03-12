8198c35f28e6eff41a59ba6dbac488735172317e
aglevillayer <levillayer.annegaelle@gmail.com>
2025-03-12 15:34:59 +0100

    refacto server initialization

--- server.ts
+++ server.ts
@@ -9,14 +9,22 @@ const app = Fastify({
 
 // Declare a route
 app.get('/', async function handler(request, reply) {
-    return "Hello World"
+    return "Welcome on PDF generator"
+})
+
+app.get('/api/pdf', async function handler(request, reply) {
+    return "Give me your url"
 })
 
 // Run the server!
-try {
-    app.listen({ port: port })
-    console.log(`server is running on port ${port} 🚀`)
-} catch (err) {
-    app.log.error(err)
-    process.exit(1)
+const start = async () => {
+    try {
+        await app.listen({ port: port })
+        console.log(`server is running on port ${port} 🚀`)
+    } catch (err) {
+        app.log.error(err)
+        process.exit(1)
+    }
 }
+
+start()
