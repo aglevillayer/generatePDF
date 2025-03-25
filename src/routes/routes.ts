e95e7f274fe412faf2573ffee0e74555aceecdf0
aglevillayer <levillayer.annegaelle@gmail.com>
2025-03-25 15:12:51 +0100

    test for prod app

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -7,7 +7,7 @@ async function routes(app: FastifyInstance) {
     // route /
     app.get('/', async function handler() {
         console.log("Route /")
-        return "Welcome on PDF generator, feel free to use it for ultipay"
+        return "Welcome on PDF generator"
     });
 
     // route /api/pdf
