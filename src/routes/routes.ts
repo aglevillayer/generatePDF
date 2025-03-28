dbeeddb21c23a73e024c18e331194702e3770247
aglevillayer <levillayer.annegaelle@gmail.com>
2025-03-28 14:32:31 +0100

    decode and decrypt token to get url

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,8 +1,28 @@
 import { FastifyInstance, FastifyRequest } from "fastify";
-import { chromium } from "playwright"
+import { chromium } from "playwright";
+import crypto from "crypto";
 
 async function routes(app: FastifyInstance) {
-    type UrlParamsType = { url: String }
+    type UrlParamsType = { token: string }
+
+    // A VIRER : à faire dans le client utilisant l'API
+    // crypte url
+    const algorithm = 'aes-256-cbc';
+    const secretKey = process.env.ENCRYPT_KEY || 'mypassword';
+    console.log("secretKey")
+    console.log(secretKey)
+    const key = crypto.scryptSync(secretKey, 'salt', 32);
+    const iv = Buffer.alloc(16, 0);
+
+    const strToEncrypt = 'https://www.wikipedia.fr';
+    const cipher = crypto.createCipheriv(algorithm, key, iv);
+    let mystrEncrypted = cipher.update(strToEncrypt, 'utf8', 'hex')
+    mystrEncrypted += cipher.final('hex');
+
+    // encoded cryptedURL in base64
+    const encryptedUrlBase64 = btoa(encodeURIComponent(mystrEncrypted))
+    console.log("encryptedUrlBase64")
+    console.log(encryptedUrlBase64)
 
     // route /
     app.get('/', async function handler() {
@@ -11,17 +31,32 @@ async function routes(app: FastifyInstance) {
 
     // route /api/pdf
     app.get('/api/pdf', async (request: FastifyRequest<{ Querystring: UrlParamsType }>) => {
-        // get the url given in query
-        const { url } = request.query;
-        if (!url) {
+        // get the token given in query
+        const { token } = request.query;
+        if (!token) {
             return "Please, indicate the url to pdf"
         }
+
+        // decode base64
+        const urlEncrypted = decodeURIComponent(atob(token));
+
+        // decrypte token to get url
+        const algorithm = 'aes-256-cbc';
+        const secretKey = process.env.ENCRYPT_KEY || 'mypassword';
+        const key = crypto.scryptSync(secretKey, 'salt', 32);
+        const iv = Buffer.alloc(16, 0);
+
+        const decipher = crypto.createDecipheriv(algorithm, key, iv);
+
+        let url = decipher.update(urlEncrypted, 'hex', 'utf8');
+        url += decipher.final('utf8');
 
         // ckeck if URL is valid
         const urlRegex = new RegExp("(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})")
         if (!url.match(urlRegex)) {
             throw new Error("Invalid URL");
         }
+        console.log("valid url")
 
         // launch the browser and open a new blank page
         const browser = await chromium.launch()
